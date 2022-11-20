84ce3465b967364bb3a446106a03f8f348e9b225
Artur Maciejczyk <maciejczyk.art@gmail.com>
2022-11-21 00:11:31 +0100

    rename constant to better reflect purpose

--- PNG/header/Chunk.h
+++ PNG/header/Chunk.h
@@ -3,10 +3,10 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define TYPE_SIZE 4
+#define TYPE_BYTE_COUNT 4
 typedef struct {
 	uint32_t Length;
-	uint8_t Type[TYPE_SIZE];
+	uint8_t Type[TYPE_BYTE_COUNT];
 	uint8_t* Data;
 	uint32_t CRC;
 	bool IsValid;
